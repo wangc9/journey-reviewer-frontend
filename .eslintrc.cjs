44e524a3b3ef067b3cdce3a29397de3d891a471d
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-11-20 11:41:08 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+    "plugin:cypress/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules", ".github"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "jest"],
+  rules: { "react/jsx-no-target-blank": "off" },
+}
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
